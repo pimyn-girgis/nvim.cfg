6c16e93488adee7dbc191a434bd88c025cb4d7e6
Pimyn Girgis <bemenboshra2001@gmail.com>
2024-11-14 12:16:20 +0200

    remove duplicate luasnip

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -27,8 +27,7 @@ return { -- Autocompletion
       end
     },
     event = 'InsertEnter',
-    'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path',
   },
   config = function()
     local cmp = require('cmp')
